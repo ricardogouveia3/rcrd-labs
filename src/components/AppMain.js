5a9350e1cfad0aa69da9dcb02ffb1dedf6b9d79b
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-05-03 19:51:08 -0300

    Fetch data from RCRD api

--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -1,37 +1,27 @@
 import React from 'react';
 
+// Components
 import AppLabItem from './cards/AppLabItem'
 
 // Assets
 import './AppMain.sass';
+
+// Consts
+const API_LABS_ENDPOINT = 'http://api.rcrd.me/rcrd/labs';
 
 class AppMain extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      labItems: [
-        {
-          title: "AMUT",
-          color: "#E74C3C",
-          link: "https://rcrd.me/amut/",
-          description: "Teclado virtual de memes em som"
-        },
-        {
-          title: "MPG",
-          color: "#4A4A4A",
-          link: "https://rcrd.me/mpg/",
-          description: "Gerador de senhas amigáveis e seguras"
-
-        },
-        {
-          title: "LBCM",
-          color: "#FFC355",
-          link: "https://rcrd.me/lbcm/",
-          description: "Máquina de criptografia baseada no comprimento do texto"
-        }
-      ]
+      labItems: []
     }
+  }
+
+  componentDidMount() {
+    fetch(API_LABS_ENDPOINT)
+      .then(response => response.json())
+      .then(data => this.setState({ labItems: data }));
   }
 
   render() {
