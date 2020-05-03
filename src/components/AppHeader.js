c7a5f5b7a9235763186285d9e5515ea315b9a11b
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-05-03 07:33:40 -0300

    Header done

---
+++ src/components/AppHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// Assest
+import './AppHeader.sass';
+import logo from './../assets/logo-w.svg';
+
+class AppHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <header>
+        <a href="/" className="content">
+          <h1>
+            <span>LABS</span>
+            <img src={logo} alt="" />
+          </h1>
+        </a>
+      </header>
+    );
+  }
+}
+
+export default AppHeader;
