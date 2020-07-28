7cdf592c99980fa7a0db3fede10953e5d842b0e4
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-07-28 09:42:51 -0300

    596896f

---
+++ precache-manifest.36a8fdf1462a76cf8198630272589a0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366b30b73ca7db0e930d92b95965d705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f5ef5c7745d640567a",
+    "url": "/static/css/main.040ad806.chunk.css"
+  },
+  {
+    "revision": "9cb120e7d70b678e5d3b",
+    "url": "/static/js/2.5da9462a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5da9462a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f5ef5c7745d640567a",
+    "url": "/static/js/main.a7f655a8.chunk.js"
+  },
+  {
+    "revision": "cc259d0c1f8428940b83",
+    "url": "/static/js/runtime-main.4af5d9bb.js"
+  },
+  {
+    "revision": "204e38786e44b1d0965e65d3971cd05e",
+    "url": "/static/media/logo-w.204e3878.svg"
+  }
+]);
